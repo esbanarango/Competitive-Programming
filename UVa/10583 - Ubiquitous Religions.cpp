c75e08f82f3928ca50ef9a73c5770659275b7b90
Esteban Arango Medina <marranoparael31@gmail.com>
2016-04-27 09:52:57 -0500

    uva 10583

---
+++ UVa/10583 - Ubiquitous Religions.cpp
@@ -0,0 +1,77 @@
+/*
+  Esteban Arango Medina
+*/
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <climits>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <bitset>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<string,int> si;
+typedef pair<int,ii> iii;
+typedef vector <si> vsi;
+typedef vector <ii> vii;
+typedef vector <int> vi;
+typedef vector <char> vc;
+typedef vector <string> vs;
+typedef map <string,vs> msvs;
+typedef map <string,int> msi;
+typedef map <string,string> mss;
+#define INF 1000000000
+
+
+class UFDS {
+private:
+  vi p, rank, setSize;
+  int numSets;
+public:
+  UFDS(int N) {
+    setSize.assign(N, 1); numSets = N; rank.assign(N, 0);
+    p.assign(N, 0); for (int i = 0; i < N; i++) p[i] = i; }
+  int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
+  bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
+  void unionSet(int i, int j) {
+    if (!isSameSet(i, j)) { numSets--;
+    int x = findSet(i), y = findSet(j);
+    // rank is used to keep the tree short
+    if (rank[x] > rank[y]) { p[y] = x; setSize[x] += setSize[y]; }
+    else                   { p[x] = y; setSize[y] += setSize[x];
+                             if (rank[x] == rank[y]) rank[y]++; } } }
+  int numDisjointSets() { return numSets; }
+  int sizeOfSet(int i) { return setSize[findSet(i)]; }
+};
+
+int main(){
+  // freopen("in.in", "r", stdin);
+  // freopen("out.out", "w", stdout);
+  int n,m,i,j,cases;
+  cases = 1;
+  while(scanf("%d %d",&n,&m) && n !=0 && m != 0) {
+  	UFDS UF(n);
+  	while(m--) {
+  		scanf("%d %d",&i,&j);
+  		UF.unionSet(i,j);
+  	}
+  	printf("Case %d: %d\n",cases++, UF.numDisjointSets());
+  }
+ return 0;
+
+}
