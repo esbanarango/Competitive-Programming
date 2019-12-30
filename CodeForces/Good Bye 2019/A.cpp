0336f5f7b6182f432e08eb36f2af32458eca49ad
Esteban Arango Medina <marranoparael31@gmail.com>
2019-12-30 01:09:44 -0500

    CF - Good bye 2019

---
+++ CodeForces/Good Bye 2019/A.cpp
@@ -0,0 +1,77 @@
+/*
+  Esteban Arango Medina
+*/
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <climits>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <bitset>
+#include <cmath>
+#include <queue>
+#include <tuple>
+#include <deque>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<string,int> si;
+typedef pair<int,ii> iii;
+typedef vector <si> vsi;
+typedef vector <ii> vii;
+typedef vector <int> vi;
+typedef vector <char> vc;
+typedef vector <string> vs;
+typedef map <string,vs> msvs;
+typedef map <string,int> msi;
+typedef map <char,int> mci;
+typedef map <int,int> mii;
+typedef map <string,string> mss;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+#define endl '\n' // Normal `cout << endl` flushes the output every time wich hit performance badly
+#define INF 1000000000
+#define deb(x) cout<<#x<<": "<<x<<endl;
+#define printArray(arr) for(auto x: arr) { cout<<x<<", "; }
+#define printMatrix(mat) for(auto x: mat) { cout<<" "; printArray(x); cout<<endl; }
+#define printMap(mmap) for(auto p: mmap) { cout<< p.first<<": "<<p.second<<endl; }
+
+int main(){
+  ios_base::sync_with_stdio(false); cin.tie(NULL); // Fast input
+  #ifdef LOCAL_PROJECT
+    freopen("in.in", "r", stdin);
+    // freopen("out.out", "w", stdout);
+  #endif
+
+  int T; cin>>T;
+  while(T--) {
+    int n, k1, k2, maxK1 = 0, maxK2 = 0;
+    cin>>n>>k1>>k2;
+    int card;
+    rep(i, 0, k1) {
+      cin>>card;
+      maxK1 = max(maxK1, card);
+    }
+    rep(i, 0, k2) {
+      cin>>card;
+      maxK2 = max(maxK2, card);
+    }
+    (maxK1 > maxK2) ? cout<<"YES" : cout<<"NO";
+    cout<<endl;
+  }
+  return 0;
+}
---
+++ CodeForces/Good Bye 2019/B.cpp
@@ -0,0 +1,80 @@
+/*
+  Esteban Arango Medina
+*/
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <climits>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <bitset>
+#include <cmath>
+#include <queue>
+#include <tuple>
+#include <deque>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<string,int> si;
+typedef pair<int,ii> iii;
+typedef vector <si> vsi;
+typedef vector <ii> vii;
+typedef vector <int> vi;
+typedef vector <char> vc;
+typedef vector <string> vs;
+typedef map <string,vs> msvs;
+typedef map <string,int> msi;
+typedef map <char,int> mci;
+typedef map <int,int> mii;
+typedef map <string,string> mss;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+#define endl '\n' // Normal `cout << endl` flushes the output every time wich hit performance badly
+#define INF 1000000000
+#define deb(x) cout<<#x<<": "<<x<<endl;
+#define printArray(arr) for(auto x: arr) { cout<<x<<", "; }
+#define printMatrix(mat) for(auto x: mat) { cout<<" "; printArray(x); cout<<endl; }
+#define printMap(mmap) for(auto p: mmap) { cout<< p.first<<": "<<p.second<<endl; }
+
+int main(){
+  ios_base::sync_with_stdio(false); cin.tie(NULL); // Fast input
+  #ifdef LOCAL_PROJECT
+    freopen("in.in", "r", stdin);
+    // freopen("out.out", "w", stdout);
+  #endif
+
+  int T; cin>>T;
+  while(T--) {
+    int n;
+    cin>>n;
+    vector<ll> A(n);
+    rep(i, 0, n) { cin>>A[i]; }
+    bool valid = false;
+    int l = 0, r = 1;
+    rep(i, 1, n) {
+      if(abs(A[i-1] - A[i]) > 1) { valid = true; break; }
+      l++; r++;
+    }
+    if(valid){
+      cout<<"YES"<<endl;
+      cout<<l+1<<" "<<r+1<<endl;
+    } else {
+      cout<<"NO"<<endl;
+    }
+  }
+  return 0;
+}
