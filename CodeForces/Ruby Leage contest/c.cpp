7f371f2608bb30a4375b7fff718247366b891ba2
Esteban Arango Medina <esteban.arango@envoy.com>
2020-04-12 22:29:55 -0500

    atcoder and codeforeces

---
+++ CodeForces/Ruby Leage contest/c.cpp
@@ -0,0 +1,97 @@
+/*
+  Esteban Arango Medina
+*/
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <climits>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <bitset>
+#include <cmath>
+#include <queue>
+#include <tuple>
+#include <deque>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+using namespace std;
+
+using ll=long long;
+#define int ll
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<string,int> si;
+typedef pair<int,ii> iii;
+typedef vector <si> vsi;
+typedef vector <ii> vii;
+typedef vector <int> vi;
+typedef vector <char> vc;
+typedef vector <string> vs;
+typedef map <string,vs> msvs;
+typedef map <string,int> msi;
+typedef map <char,int> mci;
+typedef map <int,int> mii;
+typedef map <string,string> mss;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+#define endl '\n' // Normal `cout << endl` flushes the output every time wich hit performance badly
+#define INF 1000000000
+#define deb(x) cout<<#x<<": "<<x<<endl;
+#define printArray(arr) for(auto x: arr) { cout<<x<<", "; }
+#define printMatrix(mat) for(auto x: mat) { cout<<" "; printArray(x); cout<<endl; }
+#define printMap(mmap) for(auto p: mmap) { cout<< p.first<<": "<<p.second<<endl; }
+
+signed main(){
+  ios_base::sync_with_stdio(false); cin.tie(NULL); // Fast input
+  #ifdef LOCAL_PROJECT
+    freopen("in.in", "r", stdin);
+    // freopen("out.out", "w", stdout);
+  #endif
+
+  int T = 1; cin>>T;
+  while(T--) {
+    int n; cin>>n;
+    string s;
+    cin>>s;
+    map<pair<int, int>, int> currentCycle;
+    pair<int, int> currentPos = {0,0};
+    currentCycle[currentPos] = -1;
+
+    int total = INT_MAX;
+    pair<int, int> ans = {-1,-1};
+    rep(i, 0, n){
+      char pos = s[i];
+      if(pos == 'U'){currentPos.second++; }
+      if(pos == 'D'){currentPos.second--; }
+      if(pos == 'R'){currentPos.first++; }
+      if(pos == 'L'){currentPos.first--; }
+      if(currentCycle.count(currentPos)){
+        int length = (i+1) - (currentCycle[currentPos] +1) + 1;
+        if(length < total){
+          total = length;
+          ans = {currentCycle[currentPos]+2,  i+1};
+        }
+      }
+      currentCycle[currentPos] = i;
+    }
+    if(ans.first != -1) {
+      cout<<ans.first<<" "<<ans.second<<endl;
+     } else {
+      cout<<-1<<endl;
+     }
+  }
+  return 0;
+}
